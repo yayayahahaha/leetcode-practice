da01b00795dc3161ce0e53e72af5f861281be110
flyc <yayayahahahaooii@gmail.com>
2022-04-12 19:12:01 +0800

    refactore countYourDay

--- countYourDay.js
+++ countYourDay.js
@@ -1,16 +1,25 @@
-function countYourDay(date, name) {
-  const dateValue = new Date(date).valueOf()
-  const day = 1000 * 60 * 60 * 24
-  const now = Date.now()
+function countYourDay(list) {
+  list
+    .sort((a, b) => new Date(a.date).valueOf() - new Date(b.date).valueOf())
+    .forEach(({ date, name }) => consoleDate(date, name))
 
-  const result = (Math.round(((now - dateValue) * 100) / day) / 100).toFixed(2)
+  function consoleDate(date, name) {
+    const dateValue = new Date(date).valueOf()
+    const day = 1000 * 60 * 60 * 24
+    const now = Date.now()
 
-  console.log(name, `${result} 天`)
+    const workingLoooong = (Math.round(((now - dateValue) * 10) / day) / 10).toFixed(1)
+    console.log(`${name}: ${workingLoooong} 天`)
+  }
 }
 
-countYourDay('2019/9/10 9:30', 'Sean')
-countYourDay('2017/9/11 9:30', 'Flyc')
-countYourDay('2019/10/2 9:30', 'James')
-countYourDay('2020/3/7 9:30', 'Iris')
-countYourDay('2017/11/13 9:30', 'Penny')
-countYourDay('2017/2/2 9:30', 'Katherine')
+const input = [
+  { name: 'Sean', date: '2019/9/10 9:30' },
+  { name: 'Flyc', date: '2017/9/11 9:30' },
+  { name: 'James', date: '2019/10/2 9:30' },
+  { name: 'Iris', date: '2020/3/7 9:30' },
+  { name: 'Penny', date: '2017/11/13 9:30' },
+  { name: 'Katherine', date: '2017/2/2 9:30' }
+]
+
+countYourDay(input)
