cc9b1ed28849366d621351f3a2cf3f7bca362700
flyc <yayayahahahaooii@gmail.com>
2022-04-19 14:33:50 +0800

    update validate-binary-search-tree info from solution

--- validate-binary-search-tree/index.js
+++ validate-binary-search-tree/index.js
@@ -102,3 +102,15 @@ console.log(isValidBST(root2), false)
 console.log(isValidBST(root3), false)
 console.log(isValidBST(root4), false)
 console.log(isValidBST(root5), false)
+
+// 這個是別人的解答:
+const isValidBST_from_others = function (root) {
+  if (!root) return true
+  return dfs(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+
+  function dfs(root, min, max) {
+    if (!root) return true
+    if (root.val <= min || root.val >= max) return false
+    return dfs(root.left, min, root.val) && dfs(root.right, root.val, max)
+  }
+}
