cf40c9c7f92c16d9485a6941174e60fdde140afc
flyc <yayayahahahaooii@gmail.com>
2022-05-17 18:55:40 +0800

    keep tuning curring stuff. make curry with infinite chains and could pass fn into it

--- binance/infinite-currying.js
+++ binance/infinite-currying.js
@@ -1,3 +1,5 @@
+// 從來沒有想過會栽在 Curry 上，要稍微調整一下解題思維了
+
 // https://aw50zxj2awv3cg.netlify.app/infinite-currying.html
 function g(num) {
   const result = function (num2) {
@@ -8,22 +10,22 @@ function g(num) {
   return result
 }
 
-console.log(g(1).valueOf())
-console.log(g(1)(2).valueOf())
-console.log(g(1)(2)(3).valueOf())
-console.log(g(1)(2)(3)(4).valueOf())
+// console.log(g(1).valueOf())
+// console.log(g(1)(2).valueOf())
+// console.log(g(1)(2)(3).valueOf())
+// console.log(g(1)(2)(3)(4).valueOf())
 
 // Garena Booyah 的面試題
-const add = (a, b) => a + b
+const add = (a = 0, b = 0) => a + b
 
 const curry = function (fn) {
   // 中間那些換名字的宣告項可以不用寫
-  return function (num1) {
+  return function curryAdd(num1) {
     const add = fn
     const a = num1
-    return function (num2) {
+    return function addFive(num2) {
       const b = num2
-      return fn(a, b)
+      return fn(a, b) // result
     }
   }
 }
@@ -31,29 +33,41 @@ const curry = function (fn) {
 const curryAdd = curry(add)
 const addFive = curryAdd(5)
 const result = addFive(3)
-
+const result2 = addFive(4)
 // console.log(result)
+// console.log(result2)
 
 function recursiveCurry(fn) {
-  let accum = 0
+  return function infiniteAdd(num) {
+    let accum
+    accum = fn(num, accum)
+    stepFn.just = accum
 
-  function more(num) {
-    accum = fn(accum, num)
-    more.just = accum // 如果不要用 function 的話，要在這個階段把直重新綁到 more 上，這樣的話在外面使用的時候才會是對的
-    return more
+    function stepFn(num2) {
+      accum = fn(num2, accum)
+      stepFn.just = accum
+
+      return stepFn
+    }
+
+    return stepFn
   }
-  more.valueOf = () => accum // 這個的話因為在執行的時候才會去取，所以會即時 closure 去找到上面的
-
-  return more
 }
 
 const infiniteAdd = recursiveCurry(add)
+console.log(infiniteAdd(13).just)
+console.log(infiniteAdd(1)(2)(3).just)
+console.log(infiniteAdd(3)(4).just)
+console.log(infiniteAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10).just)
 
-let recursiveResult = infiniteAdd(1)(2)(3).just
-console.log(recursiveResult)
+function ccc(input1) {
+  let accum
+  function result(input2) {
+    accum = input1 + input2
+    return ccc(accum)
+  }
+  result.valueOf = () => accum
+  return result
+}
 
-recursiveResult = infiniteAdd(3)(4).just
-console.log(recursiveResult)
-
-recursiveResult = infiniteAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10).just
-console.log(recursiveResult)
+console.log('result', ccc(1).valueOf())
